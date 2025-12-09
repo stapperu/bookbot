7d3c6a779e0506761f683614b84ff8ce90cd51aa
stapperu <k.kubiak94@gmail.com>
2025-12-09 17:16:59 +0100

    word/characters counter for books(or text)

---
+++ main.py
@@ -0,0 +1,17 @@
+from stats import get_book_text
+from stats import count_characters
+from stats import count_words
+from stats import order_list
+import sys
+
+def main():
+
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	else:
+		book_path= sys.argv[1]
+	word_count=count_words(book_path)
+	orderlist=order_list(book_path)
+
+main()
---
+++ stats.py
@@ -0,0 +1,57 @@
+def get_book_text(path):
+        with open(path) as f:
+                return f.read()
+        
+def count_words(path):
+        with open(path) as f:
+                text=f.read()
+                split_text= text.split()
+        count=len(split_text)
+        return count
+
+def count_characters(path):
+	new_dict={}
+	with open(path) as f:
+		text=f.read()
+		split_text=text.split()
+		for word in split_text:
+			for ch in word:
+				low_char=ch.lower()
+				if low_char in new_dict:
+					new_dict[low_char]+=1
+				else:
+					new_dict[low_char]=1
+	count=len(new_dict)                          
+	return count
+
+# ch_dict - {} {"char":"a", "num":"23"}  }
+# mam ch_dict - {"char":[num],             }
+def order_list(path):
+    new_dict = {}
+    count=count_words(path)
+    with open(path) as f:
+        text = f.read()
+        split_text = text.split()
+        for word in split_text:
+            for ch in word:
+                ch_dict = {}
+                low_char = ch.lower()
+                if low_char in new_dict:
+                    new_dict[low_char]["num"] += 1
+                else:
+                    ch_dict["char"] = low_char
+                    ch_dict["num"] = 1
+                    new_dict[low_char] = ch_dict
+    
+    result = list(new_dict.values())
+    result.sort(key=lambda x: x["num"], reverse=True)
+    
+    print(" ============ BOOKBOT ============")
+    print(f" Analyzing book found at {path}...")
+    print(" ----------- Word Count ----------")
+    print(f" Found {count} total words")
+    print(" --------- Character Count -------")
+
+    for item in result:
+        print(f" {item['char']}: {item['num']}")
+    return count
